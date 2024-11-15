3b9090cf2eb5768727597fad609346fb0f8ea648
Jose Ortega <jmortegafreire@gmail.com>
2024-11-15 14:57:35 -0300

    changing to https to use with githubpages

--- src/main/java/com/aluracursos/forohub/config/CorsConfiguration.java
+++ src/main/java/com/aluracursos/forohub/config/CorsConfiguration.java
@@ -11,7 +11,6 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:3000")
-//                .allowedOrigins("http://192.168.0.16:8080")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS",
                         "HEAD","TRACE","CONNECT")
                 .allowedHeaders("Authorization", "Content-Type")
